da253ea754e6a00fbfb39d5eed10c9711bc41052
anaisalfonsi <alfonsi_anais@hotmail.fr>
2021-07-21 15:33:38 +0200

    JMS Serializer course done

---
+++ src/Serializer/Handler/ArticleHandler.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Serializer\Handler;
+
+use App\Entity\Article;
+use JMS\Serializer\Context;
+use JMS\Serializer\GraphNavigator;
+use JMS\Serializer\Handler\SubscribingHandlerInterface;
+use JMS\Serializer\JsonSerializationVisitor;
+
+class ArticleHandler implements SubscribingHandlerInterface
+{
+    public static function getSubscribingMethods()
+    {
+        return [
+            [
+                'direction' => GraphNavigator::DIRECTION_SERIALIZATION,
+                'format' => 'json',
+                'type' => 'App\Entity\Article',
+                'method' => 'serialize',
+            ],
+            [
+                'direction' => GraphNavigator::DIRECTION_DESERIALIZATION,
+                'format' => 'json',
+                'type' => 'App\Entity\Article',
+                'method' => 'deserialize',
+            ]
+        ];
+    }
+
+    public function serialize(JsonSerializationVisitor $visitor, Article $article, array $type, Context $context)
+    {
+        $date = new \Datetime();
+
+        $data = [
+            'title' => $article->getTitle(),
+            'content' => $article->getContent(),
+            'serialized_at' => $date->format('l jS \of F Y h:i:s A'),
+        ];
+
+        return $visitor->visitArray($data, $type, $context);
+    }
+
+    public function deserialize(JsonSerializationVisitor $visitor, $data)
+    {
+        return new Article($data);
+    }
+
+}
